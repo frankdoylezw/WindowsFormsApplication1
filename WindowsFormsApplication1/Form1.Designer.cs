476af81172355333eb2ad853694ea95044009aff
Frank Doyle <frankdoylezw@gmail.com>
2018-01-18 22:59:08 +0000

    Altered form layout slightly

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(99, 138);
+            this.button1.Location = new System.Drawing.Point(157, 181);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(157, 81);
             this.button1.TabIndex = 0;
@@ -44,7 +44,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(385, 138);
+            this.button2.Location = new System.Drawing.Point(370, 85);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(352, 272);
             this.button2.TabIndex = 1;
