d4712f9a223bbda32513563e76c0376327c3a525
Frank Doyle <frankdoylezw@gmail.com>
2018-01-18 22:53:27 +0000

    Changed background colour of the main form

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -56,9 +56,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(877, 464);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.HelpButton = true;
             this.Name = "Form1";
             this.Text = "Practice User Form";
             this.Load += new System.EventHandler(this.Form1_Load);
